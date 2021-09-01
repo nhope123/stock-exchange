88f4b3e4fc68c66aab2b2ebd90fb6a91eb5d3af5
nhope <nhope@ryerson.ca>
2021-09-01 02:52:12 -0400

    Creating sample data

--- src/components/TimeSeriesChart.js
+++ src/components/TimeSeriesChart.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {
+	Charts,
+	ChartContainer,
+	ChartRow,
+	YAxis,
+	LineChart,
+	Resizable
+} from 'react-timeseries-charts'
 
 const TimeSeriesChart = () => {
 	return (
 		<section >
-				
+			<Resizable >
+				<ChartContainer timeRange={}  >
+					<ChartRow  height={'350'}>
+						<YAxis id={'goldAxis'} label={'Gold Cost ($)'} min={0} max={5000} type={'log'} format={'$,f'} />
+						<Charts >
+							<LineChart axis={'goldAxis'} series={} column={} />
+						</Charts>
+					</ChartRow>
+				</ChartContainer>
+			</Resizable>				
 		</section >
 	)
 }
